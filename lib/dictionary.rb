f9d8b22eb426690a7642a2e61d9a782513e36b57
Yoni190 <yfisuhu@gmail.com>
2024-08-27 12:48:14 +0300

    Create a method to choose the secret word

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -3,9 +3,16 @@ class Dictionary
 
   def initialize
     self.words = load_dictionary.split(" ")
+    self.secret_word = choose_secret_word.sample
+    puts secret_word
   end
 
   def load_dictionary
     File.read("google-10000-english-no-swears.txt")
+  end
+
+  def choose_secret_word
+    valid_words = words.filter{|word| word.length.between?(5,12)}
+    valid_words
   end
 end
